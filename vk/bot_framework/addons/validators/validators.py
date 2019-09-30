916454ff565bdd2834dde54efb47124d194a5758
prostomarkeloff <botyavs@gmail.com>
2019-09-30 20:39:19 +0300

    now you can pass own answers to bad argument

--- vk/bot_framework/addons/validators/validators.py
+++ vk/bot_framework/addons/validators/validators.py
@@ -5,34 +5,46 @@ from vk.types import Message
 vk = VK.get_current(no_error=False)
 api = vk.get_api()
 
+validators_answers: dict = {"valid_id": "", "positive_number": ""}
 
-async def valid_id(arg: str, _: Message):
+
+async def valid_id(arg: str, message: Message):
     """
     Validate passed in message ID.
     This validator just search user in VK which have this ID.
-    :param _:
+    :param message:
     :param arg:
     :return:
     """
+    have_answer = validators_answers["valid_id"]
     if not arg.isdigit():
+        if have_answer:
+            await message.answer(have_answer)
         return False
     try:
         result = await vk.api_request("users.get", {"user_ids": arg})
     except APIException:
         return False
+    if not result and have_answer:
+        await message.answer(have_answer)
     return result
 
 
-def positive_number(arg: str):
+async def positive_number(arg: str, message: Message):
     """
     Validate passed in message digit.
     If digit is positive - returns true.
+    :param message:
     :param arg:
     :return:
     """
+    have_answer = validators_answers["positive_number"]
     if not arg.isdigit():
+        if have_answer:
+            await message.answer(have_answer)
         return False
-    arg = int(arg)
-    if arg <= 0:
+    if int(arg) <= 0:
+        if have_answer:
+            await message.answer(have_answer)
         return False
     return True

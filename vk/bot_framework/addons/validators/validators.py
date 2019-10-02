f39b58abcf57dcbb075bb319a262d3b87ef5105d
prostomarkeloff <botyavs@gmail.com>
2019-10-02 19:02:24 +0300

    new validators!

--- vk/bot_framework/addons/validators/validators.py
+++ vk/bot_framework/addons/validators/validators.py
@@ -2,6 +2,7 @@ from vk import VK
 from vk.exceptions import APIException
 from vk.types import Message
 from vk.types import User
+from vk.types.community import Community
 from vk.utils import ContextInstanceMixin
 
 
@@ -9,7 +10,12 @@ class Validator(ContextInstanceMixin):
     def __init__(self, vk: VK = None):
         self._vk = vk if vk else VK.get_current()
         self._api = vk.get_api()
-        self.validators_answers: dict = {"valid_vk_id": "", "positive_number": ""}
+        self.validators_answers: dict = {
+            "valid_vk_id": "",
+            "positive_number": "",
+            "valid_vk_screenname": "",
+            "valid_vk_group_id": "",
+        }
 
     @property
     def vk(self):
@@ -19,12 +25,58 @@ class Validator(ContextInstanceMixin):
     def api(self):
         return self._api
 
+    async def valid_vk_group_id(self, arg: str, message: Message):
+        """
+        Validate passed in message group_id or screenname.
+        Just like 'valid_vk_id', but with group_id or screenname.
+
+        If all good - append to data received response from VKAPI in field 'valid_vk_group_id_group'.
+        :param arg:
+        :param message:
+        :return:
+        """
+        have_answer = self.validators_answers["valid_vk_group_id"]
+        try:
+            result = await self.vk.api_request("groups.getById", {"group_id": arg})
+        except APIException:
+            if have_answer:
+                await message.answer(have_answer)
+            return False
+        if not result and have_answer:
+            await message.answer(have_answer)
+        if result:
+            return {"valid_vk_group_id_group": Community(**result[0])}
+        return result
+
+    async def valid_vk_screenname(self, arg: str, message: Message):
+        """
+        Validate passed in message ScreenName.
+        Just like 'valid_vk_id', but with screenname.
+
+        If all good - append to data received response from VKAPI in field 'valid_vk_screenname_user'.
+        :param arg:
+        :param message:
+        :return:
+        """
+        have_answer = self.validators_answers["valid_vk_screenname"]
+        try:
+            result = await self.vk.api_request("users.get", {"user_ids": arg})
+        except APIException:
+            if have_answer:
+                await message.answer(have_answer)
+            return False
+        if not result and have_answer:
+            await message.answer(have_answer)
+        if result:
+            return {"valid_vk_screenname_user": User(**result[0])}
+        return result
+
     async def valid_vk_id(self, arg: str, message: Message):
         """
         Validate passed in message ID.
         This validator just search user in VK which have this ID.
 
-        If all good - append to data received response from VKAPI in field 'valid_id_user'.
+        If all good - append to data received response from VKAPI in field 'valid_vk_id_user'.
         Example:
 
         @dp.message_handler(commands=["hello"], have_args=[validators.valid_vk_id])
--- vk/types/__init__.py
+++ vk/types/__init__.py
@@ -1,6 +1,7 @@
 from . import additional
 from . import attachments
 from .base import BaseModel
+from .community import Community
 from .events.community.events_list import Event as BotEvent
 from .message import Action
 from .message import Message
